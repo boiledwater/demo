d153400486e78253ef1a4ea48ca13ec4db22c462
胡立忠 <lizhong.hu@meimingjia.com>
2016-07-13 17:30:05 +0800

    scrollview

---
+++ app/src/main/java/com/demo/scrollview/ScrollViewActivity.java
@@ -0,0 +1,14 @@
+package com.demo.scrollview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by HuLiZhong on 2016/6/8.
+ */
+public class ScrollViewActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/demo/scrollview/ScrollViewExt.java
@@ -0,0 +1,37 @@
+package com.demo.scrollview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+/**
+ * Created by HuLiZhong on 2016/6/8.
+ */
+public class ScrollViewExt extends ScrollView {
+    public ScrollViewExt(Context context) {
+        super(context);
+    }
+
+    public ScrollViewExt(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScrollViewExt(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+}
