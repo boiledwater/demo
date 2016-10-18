8352218bfa98d8e3a677a674244139e5abd605de
胡立忠 <lizhong.hu@meimingjia.com>
2016-10-18 16:05:42 +0800

    explode

---
+++ app/src/main/java/com/demo/anim/explode/ExplodeAnimActivity.java
@@ -0,0 +1,21 @@
+package com.demo.anim.explode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.demo.R;
+
+/**
+ * Created by HuLiZhong on 2016/9/13.
+ */
+public class ExplodeAnimActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explode_main);
+    }
+
+}
