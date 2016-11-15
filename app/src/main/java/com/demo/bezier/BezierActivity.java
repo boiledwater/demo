6392d952888374ef10b7fd28254ae520504327c3
胡立忠 <lizhong.hu@meimingjia.com>
2016-11-15 18:37:25 +0800

    BezierView

---
+++ app/src/main/java/com/demo/bezier/BezierActivity.java
@@ -0,0 +1,17 @@
+package com.demo.bezier;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.demo.R;
+
+/**
+ * Created by HuLiZhong on 2016/11/15.
+ */
+public class BezierActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bezier);
+    }
+}
