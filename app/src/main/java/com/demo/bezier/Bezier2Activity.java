d859633a800d727836d406b49bd9ce05a3b59215
胡立忠 <lizhong.hu@meimingjia.com>
2016-11-15 18:48:28 +0800

    Bezier2View

--- app/src/main/java/com/demo/bezier/Bezier2Activity.java
+++ app/src/main/java/com/demo/bezier/Bezier2Activity.java
@@ -2,6 +2,7 @@ package com.demo.bezier;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.demo.R;
 
@@ -9,9 +10,25 @@ import com.demo.R;
  * Created by HuLiZhong on 2016/11/15.
  */
 public class Bezier2Activity extends Activity {
+
+    private Bezier2View bezier2View;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bezier2);
+        bezier2View = (Bezier2View) findViewById(R.id.bv);
+        findViewById(R.id.cb_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bezier2View.setMode(true);
+            }
+        });
+        findViewById(R.id.cb_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bezier2View.setMode(false);
+            }
+        });
     }
 }
