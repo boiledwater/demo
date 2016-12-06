0122cf61f4902312a6f43767f9b4308518a00321
胡立忠 <lizhong.hu@meimingjia.com>
2016-12-06 11:13:24 +0800

    permission

--- app/src/main/java/com/demo/permission/PermissionActivity.java
+++ app/src/main/java/com/demo/permission/PermissionActivity.java
@@ -1,9 +1,16 @@
 package com.demo.permission;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,15 +21,22 @@ import com.demo.R;
  */
 public class PermissionActivity extends Activity {
     private TextView tv1;
-    String[] permissions = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+    String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission);
         findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
+                AppOpsManager appOpsManager = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
+                int checkOp = appOpsManager.checkOp(AppOpsManager.OPSTR_FINE_LOCATION, android.os.Process.myUid(), getPackageName());
+                if (checkOp == AppOpsManager.MODE_IGNORED) {
+                    // 权限被拒绝了 .
+                    System.err.println("权限被拒绝了");
+                }
                 boolean checkPermission = PermissionUtil.checkPermission(PermissionActivity.this, permissions);
                 if (checkPermission) {
                     tv1.setText("已授权");
@@ -35,13 +49,15 @@ public class PermissionActivity extends Activity {
         findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ActivityCompat.requestPermissions(PermissionActivity.this, permissions, 101);
+                //请求授权
+                ActivityCompat.requestPermissions(PermissionActivity.this, permissions, 225);
             }
         });
         findViewById(R.id.btn3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean lackPermissions = PermissionUtil.lackPermissions(PermissionActivity.this, permissions);
+                boolean lackPermissions = ContextCompat.checkSelfPermission(PermissionActivity.this, permissions[0]) == PackageManager.PERMISSION_DENIED;
+                //PermissionUtil.lackPermissions(PermissionActivity.this, permissions);
                 if (lackPermissions) {
                     tv1.setText("未授权");
                 } else {
@@ -54,7 +70,7 @@ public class PermissionActivity extends Activity {
             @Override
             public void onClick(View v) {
                 boolean shouldShowRequestPermissionRationale = ActivityCompat.shouldShowRequestPermissionRationale(PermissionActivity.this, permissions[0]);
-                shouldShowRequestPermissionRationale = shouldShowRequestPermissionRationale && ActivityCompat.shouldShowRequestPermissionRationale(PermissionActivity.this, permissions[1]);
+//                shouldShowRequestPermissionRationale = shouldShowRequestPermissionRationale && ActivityCompat.shouldShowRequestPermissionRationale(PermissionActivity.this, permissions[1]);
                 if (shouldShowRequestPermissionRationale) {
                     tv1.setText("可提示授权");
                 } else {
@@ -63,5 +79,12 @@ public class PermissionActivity extends Activity {
             }
         });
         tv1 = (TextView) findViewById(R.id.tv1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        System.err.println("requestCode:" + requestCode);
+        System.err.println("permissions:" + permissions.length + ";" + permissions[0]);
+        System.err.println("grantResults:" + grantResults.length + ";" + grantResults[0]);
     }
 }
