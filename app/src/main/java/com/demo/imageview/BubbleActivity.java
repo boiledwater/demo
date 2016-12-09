c4e198de4602920b34781f87918962c59e85b812
胡立忠 <lizhong.hu@meimingjia.com>
2016-12-09 14:50:22 +0800

    bubble

---
+++ app/src/main/java/com/demo/imageview/BubbleActivity.java
@@ -0,0 +1,17 @@
+package com.demo.imageview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.demo.R;
+
+/**
+ * Created by HuLiZhong on 2016/12/9.
+ */
+public class BubbleActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bubble);
+    }
+}
